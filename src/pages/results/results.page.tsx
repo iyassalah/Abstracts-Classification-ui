4ba0592c7225ae27be8ea5f2cff62d7959f95bb5
Iyas Salah <iyas.a.salah@gmail.com>
2023-04-29 13:42:32 +0300

    Change the hardcoded values with generic code and add the fetch request to fetch the tags and for now I used a fixed mock data for testing.

--- src/pages/results/results.page.tsx
+++ src/pages/results/results.page.tsx
@@ -1,55 +1,76 @@
-import React, { useState } from "react";
-import "./results.scss";
-import { Select } from "antd";
+import React, { useEffect, useState } from 'react';
+import { Select } from 'antd';
 import Abstract from "../../components/abstract/abstract";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface IProps {}
 
 const { Option } = Select;
+const [loading] = useState(false);
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Results = (props: IProps) => {
-  const [array] = useState([
-    { abstract: "the first abstract", tag: "Ai" },
-    { abstract: "the second abstract", tag: "Chem" },
+
+TODO: once the back-end is ready this comment code can be used to fetch the whole tages from the database.
+// const Results = () => {
+//   const [options, setOptions] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [searchArray, setSearchArray] = useState([]);
+
+//   useEffect(() => {
+//     fetch('/tags')
+//       .then(response => response.json())
+//       .then(data => {
+//         setOptions(data);
+//         setLoading(false);
+//       });
+//   }, []);
+
+
+const Results = () => {
+  const [options] = useState([
+    { value: 'Ai', label: 'Ai' },
+    { value: 'Math', label: 'Math' },
+    { value: 'Chem', label: 'Chem' },
   ]);
 
-  const [searchArray, setSearchArray] = useState(array);
+  const [searchArray, setSearchArray] = useState([
+    { abstract: 'the first abstract', tag: 'Ai' },
+    { abstract: 'the second abstract', tag: 'Chem' },
+  ]);
 
-  const handleSearch = (value) => {
-    if (value === "None") {
-      setSearchArray(array);
+  const handleSearch = value => {
+    if (value === 'None') {
+      setSearchArray([
+        { abstract: 'the first abstract', tag: 'Ai' },
+        { abstract: 'the second abstract', tag: 'Chem' },
+      ]);
     } else {
-      const filteredArray = array.filter((instance) => instance.tag === value);
+      const filteredArray = searchArray.filter(instance => instance.tag === value);
       setSearchArray(filteredArray);
     }
   };
 
   return (
-    <>
-      <div className="container">
-        <h1 className="header">Here are the results</h1>
-        <Select
-          showSearch
-          placeholder="Select a tag"
-          optionFilterProp="children"
-          onChange={handleSearch}
-          filterOption={(input, option) =>
-            (option?.children as unknown as string)?.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          <Option value="None">None</Option>
-          <Option value="Ai">Ai</Option>
-          <Option value="Math">Math</Option>
-          <Option value="Chem">Chem</Option>
-        </Select>
-
-        {searchArray.map((instance, index) => {
-          return <Abstract value={instance} key={index} />;
-        })}
-      </div>
-    </>
+    <div className="container">
+      <h1 className="header">Here are the results</h1>
+      <Select
+        showSearch
+        placeholder="Select a tag"
+        optionFilterProp="children"
+        onChange={handleSearch}
+        filterOption={(input, option) =>
+          (option?.children as unknown as string)?.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+        loading={loading}
+      >
+        <Option value="None">None</Option>
+        {options.map(option => (
+          <Option key={option.value} value={option.value}>
+            {option.label}
+          </Option>
+        ))}
+      </Select>
+      {searchArray.map((instance, index) => {
+        return <Abstract value={instance} key={index} />;
+      })}
+    </div>
   );
 };
 
