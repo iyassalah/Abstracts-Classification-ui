cb899ddf795657590b06a27ad09910277ae04b73
iyassalah <iyas.a.salah@transcend.ps>
2023-05-01 19:24:55 +0300

    send a request for interactive input abstract to be classified and display the result.

--- src/pages/interactive/interactive.page.tsx
+++ src/pages/interactive/interactive.page.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./interactive.scss";
 import { Button, Input, Tag } from "antd";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IProps {}
 
-const tagList = ["Math", "AI", "Chem"];
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Interactive = (props: IProps) => {
+  const [abstract, setAbstract] = useState("");
+  const [tagList, setTagList] = useState(['Math', 'AI']);
+
+  const handleClassify = async () => {
+    const response = await fetch("http://localhost:8000/interactive", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        abstract,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data.categories);
+
+    // Update tagList state with response data
+    setTagList(data.categories);
+  };
+
   return (
     <>
       <div className="container">
@@ -18,16 +37,18 @@ const Interactive = (props: IProps) => {
           <Input.TextArea
             className="input"
             placeholder="Type your abstract here"
+            value={abstract}
+            onChange={(e) => setAbstract(e.target.value)}
           />
           <div className="underline"></div>
         </div>
 
         <div className="bottom-row">
-          <Button className="" type="primary">
+          <Button className="" type="primary" onClick={handleClassify}>
             Classify
           </Button>
           <div className="tags-column">
-            <div className="tags-label">Tags</div>
+            <div className="tags-label">Predicted Tags:</div>
             {tagList.map((tag) => (
               <Tag key={tag}>{tag}</Tag>
             ))}
