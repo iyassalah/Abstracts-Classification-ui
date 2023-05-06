4d8b5b1a4fc70bbd06d6894cb60df95ed4c40401
yamenw <yamentaad@gmail.com>
2023-05-07 00:09:57 +0300

    #14 Add text search to title and abstract fields

--- src/pages/results/results-table.component.tsx
+++ src/pages/results/results-table.component.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './results-table.scss'
-import { Table, Modal, Tag } from "antd"
+import { Table, Modal, Tag, InputRef } from "antd"
 import { ColumnsType, TableProps } from "antd/es/table";
 import { FilterValue } from 'antd/es/table/interface';
 import * as Responses from '../../types/responses';
 import { IAbstract } from '../../types/shared';
+import useTextSearch from './text-search.hook';
 
 const mock: Responses.IResults = { abstracts: [{ "title": "The Effect of Climate Change on Biodiversity", "author": "John Smith", "abstract": "Climate change is one of the most pressing issues facing the world today. It is affecting many aspects of our lives, including biodiversity. In this paper, we examine the effect of climate change on biodiversity, and discuss what can be done to mitigate its impact.", "labels": ["math"] }, { "title": "Artificial Intelligence and its Impact on Employment", "author": "Jane Doe", "abstract": "Artificial Intelligence (AI) is a rapidly advancing field that has the potential to revolutionize the way we work. However, there are concerns that it will also lead to job displacement and unemployment. In this paper, we explore the impact of AI on employment and discuss strategies to address these challenges.", "labels": ["cs"] }, { "title": "The Role of Education in Economic Development", "author": "Mark Johnson", "abstract": "Education is widely recognized as a key driver of economic development. In this paper, we examine the relationship between education and economic development, and discuss the importance of investing in education to promote sustainable growth.", "labels": ["math", "cs"] }, { "title": "The Ethics of Animal Testing in Medical Research", "author": "Lisa Wong", "abstract": "Animal testing is a controversial topic in medical research. While it has contributed to many important advances in medicine, there are ethical concerns about the use of animals in research. In this paper, we explore the ethics of animal testing and discuss alternative approaches that could be used to advance medical research.", "labels": ["astro-physics"] }, { "title": "The Impact of Social Media on Mental Health", "author": "David Lee", "abstract": "Social media is a ubiquitous part of modern life, but there are concerns that it may be contributing to poor mental health outcomes. In this paper, we examine the impact of social media on mental health and discuss strategies to promote positive mental health in the digital age.", "labels": ["astro-physics"] }, { "title": "test", "author": "John Smith", "abstract": "test", "labels": ["math"] },] }
 
@@ -12,14 +13,16 @@ type State = { abstracts: IAbstract[], authors: Set<string>, labels: Set<string>
 const ResultsTable = () => {
     const [filters, setFilters] = useState<Record<"labels" | "author", FilterValue | null>>({ author: [], labels: [] });
     const [data, setData] = useState<State>({ abstracts: [], authors: new Set(), labels: new Set() });
+    const titleSearchInput = useRef<InputRef>(null);
+    const abstractSearchInput = useRef<InputRef>(null);
     useEffect(() => {
         setData({
             abstracts: mock.abstracts,
             labels: new Set(mock.abstracts.map(e => e.labels).flat()),
             authors: new Set(mock.abstracts.map(e => e.author)),
         }); // TODO: fetch from API
-    }, [])
-
+    }, []);
+    const { getColumnSearchProps, searchText } = useTextSearch<IAbstract>();
 
     const filterSet = new Set(filters.labels);
     const handleTagClick = (tag: string) => {
@@ -39,6 +42,8 @@ const ResultsTable = () => {
         {
             title: 'Title',
             dataIndex: 'title',
+            filteredValue: searchText ? [searchText] : null,
+            ...getColumnSearchProps('title', titleSearchInput),
         },
         {
             title: 'Author',
@@ -67,7 +72,9 @@ const ResultsTable = () => {
                     {record.abstract}
                 </a>
             ),
-            ellipsis: true
+            ellipsis: true,
+            filteredValue: searchText ? [searchText] : null,
+            ...getColumnSearchProps('abstract', abstractSearchInput),
         },
         {
             title: 'Labels',
@@ -97,7 +104,7 @@ const ResultsTable = () => {
             size='small'
             onChange={handleChange}
             className="results-table"
-            dataSource={data.abstracts.map((e, index) => ({ ...e, key: index }))}
+            dataSource={data.abstracts.map((e, index) => ({ ...e, key: String(index) }))}
             columns={columns}
         />
     )
---
+++ src/pages/results/text-search.hook.tsx
@@ -0,0 +1,103 @@
+import React, { useRef, useState } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import type { InputRef } from 'antd';
+import { Button, Input, Space } from 'antd';
+import type { ColumnType } from 'antd/es/table';
+import type { FilterConfirmProps } from 'antd/es/table/interface';
+import Highlighter from 'react-highlight-words';
+
+function useTextSearch<T>() {
+    type DataIndex = keyof T;
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState<DataIndex>('' as DataIndex);
+
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: (param?: FilterConfirmProps) => void,
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters: () => void) => {
+        clearFilters();
+        setSearchText('');
+    };
+
+    const getColumnSearchProps = (dataIndex: DataIndex, ref: React.RefObject<InputRef>): ColumnType<T> => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={ref}
+                    placeholder={`Search ${String(dataIndex)}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys.map(String), confirm, dataIndex)}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys.map(String), confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+                            setSearchText((selectedKeys as string[])[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => { console.log(filtered); return <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} /> },
+        onFilter: (value, record) =>
+            String(record[dataIndex])
+                .toLowerCase()
+                .includes(value.toString().toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => ref.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : text,
+    });
+    return { getColumnSearchProps, searchText };
+}
+
+export default useTextSearch;
