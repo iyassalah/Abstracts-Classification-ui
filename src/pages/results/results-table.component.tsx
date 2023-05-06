95c56f40ecc414defe09e0ddc903483419dffb7b
yamenw <yamentaad@gmail.com>
2023-05-06 16:23:13 +0300

    #14 implement labels column, add filter logic, adjust width

--- src/pages/results/results-table.component.tsx
+++ src/pages/results/results-table.component.tsx
@@ -1,23 +1,25 @@
-import { Table, Modal } from "antd"
+import './results-table.scss'
+import { Table, Modal, Tag } from "antd"
 import { ColumnsType } from "antd/es/table";
+import abstract from '../../components/abstract/abstract';
 
 type Abstract = {
     key: React.Key;
-    abstract_title: string;
+    title: string;
     author: string;
     abstract: string;
-    classification_result: string;
+    labels: string[];
 }
 
 const mock: Abstract[] = [{
-    "key": 1, "abstract_title": "The Effect of Climate Change on Biodiversity", "author": "John Smith", "abstract": "Climate change is one of the most pressing issues facing the world today. It is affecting many aspects of our lives, including biodiversity. In this paper, we examine the effect of climate change on biodiversity, and discuss what can be done to mitigate its impact.", "classification_result": "Positive"
-}, { "key": 2, "abstract_title": "Artificial Intelligence and its Impact on Employment", "author": "Jane Doe", "abstract": "Artificial Intelligence (AI) is a rapidly advancing field that has the potential to revolutionize the way we work. However, there are concerns that it will also lead to job displacement and unemployment. In this paper, we explore the impact of AI on employment and discuss strategies to address these challenges.", "classification_result": "Neutral" }, { "key": 3, "abstract_title": "The Role of Education in Economic Development", "author": "Mark Johnson", "abstract": "Education is widely recognized as a key driver of economic development. In this paper, we examine the relationship between education and economic development, and discuss the importance of investing in education to promote sustainable growth.", "classification_result": "Positive" }, { "key": 4, "abstract_title": "The Ethics of Animal Testing in Medical Research", "author": "Lisa Wong", "abstract": "Animal testing is a controversial topic in medical research. While it has contributed to many important advances in medicine, there are ethical concerns about the use of animals in research. In this paper, we explore the ethics of animal testing and discuss alternative approaches that could be used to advance medical research.", "classification_result": "Negative" }, { "key": 5, "abstract_title": "The Impact of Social Media on Mental Health", "author": "David Lee", "abstract": "Social media is a ubiquitous part of modern life, but there are concerns that it may be contributing to poor mental health outcomes. In this paper, we examine the impact of social media on mental health and discuss strategies to promote positive mental health in the digital age.", "classification_result": "Negative" }]
+    "key": 1, "title": "The Effect of Climate Change on Biodiversity", "author": "John Smith", "abstract": "Climate change is one of the most pressing issues facing the world today. It is affecting many aspects of our lives, including biodiversity. In this paper, we examine the effect of climate change on biodiversity, and discuss what can be done to mitigate its impact.", "labels": ["math"]
+}, { "key": 2, "title": "Artificial Intelligence and its Impact on Employment", "author": "Jane Doe", "abstract": "Artificial Intelligence (AI) is a rapidly advancing field that has the potential to revolutionize the way we work. However, there are concerns that it will also lead to job displacement and unemployment. In this paper, we explore the impact of AI on employment and discuss strategies to address these challenges.", "labels": ["cs"] }, { "key": 3, "title": "The Role of Education in Economic Development", "author": "Mark Johnson", "abstract": "Education is widely recognized as a key driver of economic development. In this paper, we examine the relationship between education and economic development, and discuss the importance of investing in education to promote sustainable growth.", "labels": ["math", "cs"] }, { "key": 4, "title": "The Ethics of Animal Testing in Medical Research", "author": "Lisa Wong", "abstract": "Animal testing is a controversial topic in medical research. While it has contributed to many important advances in medicine, there are ethical concerns about the use of animals in research. In this paper, we explore the ethics of animal testing and discuss alternative approaches that could be used to advance medical research.", "labels": ["astro-physics"] }, { "key": 5, "title": "The Impact of Social Media on Mental Health", "author": "David Lee", "abstract": "Social media is a ubiquitous part of modern life, but there are concerns that it may be contributing to poor mental health outcomes. In this paper, we examine the impact of social media on mental health and discuss strategies to promote positive mental health in the digital age.", "labels": ["astro-physics"] }]
 
 
 const columns: ColumnsType<Abstract> = [
     {
-        title: 'Abstract Title',
-        dataIndex: 'abstract_title',
+        title: 'Title',
+        dataIndex: 'title',
     },
     {
         title: 'Author',
@@ -25,7 +27,8 @@ const columns: ColumnsType<Abstract> = [
         filterMode: 'tree',
         filters: Array
             .from(new Set(mock.map(e => e.author)))
-            .map(author => ({ text: author, value: author }))
+            .map(author => ({ text: author, value: author })),
+        onFilter: (value, { author }) => author === value,
     },
     {
         title: 'Abstract',
@@ -34,7 +37,7 @@ const columns: ColumnsType<Abstract> = [
             <a
                 onClick={() =>
                     Modal.info({
-                        title: record.abstract_title,
+                        title: record.title,
                         content: <p>{record.abstract}</p>,
                         centered: true,
                         maskClosable: true,
@@ -46,30 +49,22 @@ const columns: ColumnsType<Abstract> = [
         ),
     },
     {
-        title: 'Classification Result',
-        dataIndex: 'classification_result',
+        title: 'Labels',
+        dataIndex: 'labels',
         filterMode: 'tree',
-        filters: [
-            {
-                text: 'Positive',
-                value: 'Positive',
-            },
-            {
-                text: 'Neutral',
-                value: 'Neutral',
-            },
-            {
-                text: 'Negative',
-                value: 'Negative',
-            },
-        ],
-        // onFilter: (value, record) => record.classification_result.startsWith(value),
+        filters: Array
+            .from(new Set(mock.map(e => e.labels).flat()))
+            .map(label => ({ text: label, value: label })),
+        render: (value, abstract) => abstract.labels.map(
+            label => <Tag key={label}>{label}</Tag>
+        ),
+        onFilter: (value, record) => typeof value === 'string' && record.labels.includes(value)
     },
 ];
 
 
 const ResultsTable = () => {
-    return <Table dataSource={mock} columns={columns} />
+    return <Table className="results-table" dataSource={mock} columns={columns} />
 }
 
 export default ResultsTable;
