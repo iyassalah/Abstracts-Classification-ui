cb5443ac2e9fe861570b297f73aea3c1237133c5
yamenw <yamentaad@gmail.com>
2023-05-11 21:39:48 +0300

    #19 Add request batching

--- src/pages/batch/multi-upload.component.tsx
+++ src/pages/batch/multi-upload.component.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { UploadOutlined } from '@ant-design/icons';
+import { InboxOutlined } from '@ant-design/icons';
 import type { UploadProps } from 'antd';
-import { Button, Upload } from 'antd';
 import { RcFile } from 'antd/es/upload';
 import axios from 'axios';
+import Dragger from 'antd/es/upload/Dragger';
 
 
 const MultiUpload: React.FC = () => {
@@ -13,26 +13,34 @@ const MultiUpload: React.FC = () => {
         const form = new FormData();
         fileList.filter(Boolean).forEach(file => form.append(`files`, file));
         axios.post('/active/proba/pdfs', form, {
-            headers: { 'Content-Type': 'multipart/form-data' }
+            headers: { 'Content-Type': 'multipart/form-data' },
         })
     }
 
     const props: UploadProps = {
+        name: 'files',
+        multiple: true,
         onChange({ file, fileList }) {
             if (file.status !== 'uploading') {
                 console.log(file, fileList);
             }
         },
         customRequest(data) {
-            if (fileList.length === 0)
-                setTimeout(uploadBatch, 1000);
             fileList.push(data.file);
-        }
+            setTimeout(uploadBatch, 1000);
+        },
     };
     return (
-        <Upload {...props}>
-            <Button icon={<UploadOutlined />}>Upload</Button>
-        </Upload>
+        <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                banned files.
+            </p>
+        </Dragger>
     )
 };
 
