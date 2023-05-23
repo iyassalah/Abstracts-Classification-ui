e4dae85acf43f4a13f92843c24032485d8163924
yamenw <yamentaad@gmail.com>
2023-05-23 09:38:14 +0300

    Add threshold filtering to results

--- src/pages/results/results.page.tsx
+++ src/pages/results/results.page.tsx
@@ -1,18 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ResultsTable from './results-table.component';
 import { ResultsContext } from '../../state/results';
 import { IAbstract } from '../../types/shared';
 import { useClasses } from '../../api/classes';
 import { Probabilities } from '../../types/responses';
+import { Input, InputNumber, Space, Typography } from 'antd';
 
 const Results = () => {
   const { state, dispatch } = useContext(ResultsContext);
   const { classes } = useClasses();
+  const [Threshhold, setThreshhold] = useState(0.9);
+  const [ClassCount, setClassCount] = useState(3);
 
   const predAlgo = (prob: Probabilities) => {
     return Object.entries(prob)
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      .filter(([name, prob]) => prob[0] > 0.9)
+      .filter(([name, prob]) => prob[0] > Threshhold)
       .map(([internalName]) => classes[internalName])
   }
 
@@ -27,6 +30,28 @@ const Results = () => {
   return (
     <div className="container">
       <h1 className="header">Here are the results</h1>
+
+      <Space>
+        <Typography.Text>
+          Threshold:
+        </Typography.Text>
+        <Input
+          name='thresh'
+          onBlur={e => setThreshhold(+e.target.value)}
+          type='number'
+          placeholder='0.9'
+        />
+        <Typography.Text>
+          Max class count:
+        </Typography.Text>
+        <InputNumber
+          name='count'
+          onBlur={e => setClassCount(+e.target.value)}
+          placeholder='5'
+          step={1}
+        />
+      </Space>
+
       <ResultsTable
         data={data}
         filters={state?.resultsPage}
