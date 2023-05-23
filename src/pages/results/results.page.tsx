f95f107a94a1cf299a7f490713232ae74568b6c4
yamenw <yamentaad@gmail.com>
2023-05-23 17:00:42 +0300

    Implement threshhold and class count filtering

--- src/pages/results/results.page.tsx
+++ src/pages/results/results.page.tsx
@@ -1,10 +1,9 @@
-import React, { useContext, useState } from 'react';
-import ResultsTable from './results-table.component';
+import { InputNumber, Space, Typography } from 'antd';
+import { useContext, useMemo, useState } from 'react';
+import { useClasses } from '../../api/classes';
 import { ResultsContext } from '../../state/results';
 import { IAbstract } from '../../types/shared';
-import { useClasses } from '../../api/classes';
-import { Probabilities } from '../../types/responses';
-import { Input, InputNumber, Space, Typography } from 'antd';
+import ResultsTable from './results-table.component';
 
 const Results = () => {
   const { state, dispatch } = useContext(ResultsContext);
@@ -12,34 +11,36 @@ const Results = () => {
   const [Threshhold, setThreshhold] = useState(0.9);
   const [ClassCount, setClassCount] = useState(3);
 
-  const predAlgo = (prob: Probabilities) => {
-    return Object.entries(prob)
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      .filter(([name, prob]) => prob[0] > Threshhold)
-      .map(([internalName]) => classes[internalName])
-  }
-
-  const data: IAbstract[] = state.fileList
+  const data: IAbstract[] = useMemo(() => state.fileList
     .map(file => ({
       abstract: file.response.inferred_abstract,
-      labels: predAlgo(file.response.pred),
       author: 'Not available',
       title: file.name,
-    }))
+
+      labels: Object.entries(file.response.pred)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        .filter(([name, prob]) => prob[0] > Threshhold)
+        .map(([internalName]) => classes[internalName])
+        .slice(0, ClassCount),
+    })
+    ), [state.fileList, Threshhold, ClassCount])
 
   return (
     <div className="container">
       <h1 className="header">Here are the results</h1>
-
       <Space style={{ paddingBottom: '12px' }}>
         <Typography.Text>
           Threshold:
         </Typography.Text>
-        <Input
+        <InputNumber
           name='thresh'
           onBlur={e => setThreshhold(+e.target.value)}
+          onPressEnter={e => setThreshhold(+e.currentTarget.value)}
           type='number'
+          defaultValue={0.9}
           placeholder='0.9'
+          min={0}
+          max={1}
         />
         <Typography.Text>
           Max class count:
@@ -47,6 +48,7 @@ const Results = () => {
         <InputNumber
           name='count'
           onBlur={e => setClassCount(+e.target.value)}
+          onPressEnter={e => setClassCount(+e.currentTarget.value)}
           placeholder='5'
           step={1}
         />
